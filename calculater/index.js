fcd41b63665f549e7ed83ca165cb940fed146a51
zain shekhani <157803688+zainshekhani54321@users.noreply.github.com>
2024-12-19 20:32:05 +0500

    Add files via upload

---
+++ calculater/index.js
@@ -0,0 +1,28 @@
+
+// calculater professional
+
+
+// function abc(e){
+//   document.getElementById('inp').value += e
+    
+// }
+// function eq(){
+//     var eq = document.getElementById("inp")
+//     eq.value=eval(eq.value)
+// }
+
+
+
+
+
+
+// function cla(){
+//     var cl = document.getElementById('inp')
+//     cl.value=""
+// }
+
+// function cl(){
+//     var a=  document.getElementById('inp')
+//     var b = a.value
+//     a.value = b.slice(0 ,-1)
+// }
